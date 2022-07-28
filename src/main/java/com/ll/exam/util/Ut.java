1691b5be7bedeecd54bccd3c4b0664777d4bd2bb
sh970901 <aaddss639@naver.com>
2022-07-28 11:25:22 +0900

    json을 객체화, 객체를 json화, 제너릭 사용

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -2,15 +2,26 @@ package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
     public static class json {
-        public static String toJsonStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
+        private static final ObjectMapper om;
 
+        static {
+            om = new ObjectMapper();
+        }
+
+        public static String toStr(Object obj, String defaultValue) {
             try {
                 return om.writeValueAsString(obj);
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+
+        public static <T> T toObj(String jsonStr, Class<T> cls, T defaultValue) {
+            try {
+                return (T) om.readValue(jsonStr, cls);
             } catch (JsonProcessingException e) {
                 return defaultValue;
             }
